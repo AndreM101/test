91976a5efee123b94a9cc742cf1cc663ad9ea775
Andre <marchmail9@gmail.com>
2022-01-20 17:34:22 +0800

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -2,11 +2,101 @@ require 'scraperwiki'
 require 'date'
 
 record = {}
-record['council_reference'] = '287855'
-record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=287855'
-record['address'] = '160 Dangar Street, ARMIDALE, NSW 2350'
-record['description'] = 'Alterations & Additions - Demolition of Existing Out Buildings, Erection of New Garage, Carport and Fencing - (Consent Modification)'
+record['council_reference'] = '295968'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=295968'
+record['address'] = '122 Barney Street, ARMIDALE, NSW 2350'
+record['description'] = 'Dual Occupancy - Demolition of Existing Dwelling & Garage and Construction of a Detached Dual Occupancy'
 record['date_scraped'] = Date.today.to_s
-record['date_received'] = '2021-05-10'
+record['date_received'] = '2021-11-24'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '283050'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=283050'
+record['address'] = '12 Ben Venue Boulevard, ARMIDALE, NSW 2350'
+record['description'] = 'Subdivision - 1 to 11 Lot Community Title Subdivision'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-02-04'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '291454'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=291454'
+record['address'] = '45 Gentles Road, ARMIDALE, NSW 2350'
+record['description'] = 'Dual Occupancy Development - One Existing Dwelling/One New Dwelling'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-08-11'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '283359'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=283359'
+record['address'] = '145 Long Swamp Road, ARMIDALE, NSW 2350'
+record['description'] = 'New Dwelling'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-02-15'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '289700'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=289700'
+record['address'] = '41LI Donnelly Street, ARMIDALE, NSW 2350'
+record['description'] = 'Demolition of Existing Amenities Block and Construction of a New Amenities Block, Administration and Canteen'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-06-26'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '286381'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=286381'
+record['address'] = '81 Translator Road, ARMIDALE, NSW 2350'
+record['description'] = 'Subdivision - 1 to 10 Lot Rural Residential Subdivision (Consent Modification - Community to Torrens Title)'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-04-14'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '288890'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=288890'
+record['address'] = '9 Willow Way, ARMIDALE, NSW 2350'
+record['description'] = 'Dual Occupancy'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-06-7'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '288900'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=288900'
+record['address'] = '9 Phoenix Lane, ARMIDALE, NSW 2350'
+record['description'] = 'Subdivision - 5 Lot Strata Subdivision'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-06-7'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '289155'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=289155'
+record['address'] = '122 Barney Street, ARMIDALE, NSW 2350'
+record['description'] = 'Dual Occupancy - Demolition of Existing Dwelling & Garage and Construction of a Detached Dual Occupancy'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-06-10'
+record['authority_label'] = 'armidale'
+ScraperWiki.save_sqlite(['council_reference'], record)
+
+record = {}
+record['council_reference'] = '288139'
+record['info_url'] = 'https://epathway.newengland.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=288139'
+record['address'] = '6 Campbell Parade, ARMIDALE, NSW 2350'
+record['description'] = 'Subdivision - Residential Subdivision of 1 into 26 Lots -(Consent Modification)'
+record['date_scraped'] = Date.today.to_s
+record['date_received'] = '2021-05-18'
 record['authority_label'] = 'armidale'
 ScraperWiki.save_sqlite(['council_reference'], record)
